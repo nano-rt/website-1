1984f41407bd9b54af5cf6c34ed0398ecff5fd69
nano-rt <jb.hasan89@gmail.com>
2026-07-25 03:04:51 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// ============================================
+// PASTE YOUR SUPABASE PROJECT DETAILS HERE
+// (See README.md — Step 1 — for where to find these)
+// ============================================
+
+const SUPABASE_URL = "https://bfuojejkzeupdaxwgyok.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_D1k2CHY96F4RElHzIu5XFw_NHZse8iS";
